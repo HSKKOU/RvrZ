cc908bb310d092b0ae847b7c31db44896b90e0af
HSKKOU <hisa.k@sde.cs.titech.ac.jp>
2016-11-11 09:59:20 +0900

    change db

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -3,7 +3,7 @@
 return array(
   'db' => array(
     'driver' => 'Pdo',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=vrshopping03;charset=utf8;',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=vrshopping;charset=utf8;',
   ),
   'service_manager' => array(
     'factories' => array(
